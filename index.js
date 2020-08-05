745a2f20c94583a9e1db9ea159cc6cf6d0d13a73
Michael Canon <michaeljamescanon@gmail.com>
2020-08-05 19:04:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
-function happyHolidayTo(holiday, name) {
+function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(holiday, days) {
